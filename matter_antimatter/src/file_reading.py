02428b46b6254012af60d81a2a32766277f68337
Aavash Subedi <87420856+aavashsubedi@users.noreply.github.com>
2023-03-07 15:48:15 +0000

    Update

--- matter_antimatter/src/file_reading.py
+++ matter_antimatter/src/file_reading.py
@@ -2,7 +2,18 @@ import numpy as np
 import uproot
 from tqdm import tqdm
 
-def read_file(path_name="", MAX_EVENTS=5000, mode=1):
+list_of_interesting_keys = []
+for i in range(1, 4):
+    exec(f"list_of_interesting_keys.append('H{i}_PX')")
+    exec(f"list_of_interesting_keys.append('H{i}_PY')  ")
+    exec(f"list_of_interesting_keys.append('H{i}_PZ')  ")
+    exec(f"list_of_interesting_keys.append('H{i}_Charge')  ")
+    exec(f"list_of_interesting_keys.append('H{i}_ProbK')  ")
+    exec(f"list_of_interesting_keys.append('H{i}_ProbPi')  ")
+    exec(f"list_of_interesting_keys.append('H{i}_isMuon')  ")
+
+
+def read_file(path_name="", MAX_EVENTS=5000, mode=1, keys = list_of_interesting_keys):
 
     if not path_name:
         path = 'data/'  # set this to '' to run on the GitHub version
@@ -54,38 +65,38 @@ def read_file(path_name="", MAX_EVENTS=5000, mode=1):
     print()
     for tree in tqdm(trees):
         # This outer loop is a technical loop of uproot over chunks of events
-        for data in tree.iterate([b'H*_P[XYZ]', b'H*_Charge', b'H*_Prob*', b'H*_isMuon']):
+        for data in tree.iterate(keys):
             # As Python can handle calculations with arrays, we can calculate derived quantities here
-            pT_H1 = np.sqrt(data[b'H1_PX']**2+data[b'H1_PY']**2)
-            pT_H2 = np.sqrt(data[b'H2_PX']**2+data[b'H2_PY']**2)
-            pT_H3 = np.sqrt(data[b'H3_PX']**2+data[b'H3_PY']**2)
+            pT_H1 = np.sqrt(data['H1_PX']**2+data['H1_PY']**2)
+            pT_H2 = np.sqrt(data['H2_PX']**2+data['H2_PY']**2)
+            pT_H3 = np.sqrt(data['H3_PX']**2+data['H3_PY']**2)
 
             # Your invariant mass calculation should go here
 
             # This loop will go over individual events
-            for i in range(0, len(data[b'H1_PZ'])):
+            for i in range(0, len(data['H1_PZ'])):
                 event_counter += 1
                 if 0 < MAX_EVENTS and MAX_EVENTS < event_counter:
                     break
                 if 0 == (event_counter % 100000):
                     print('Read', event_counter, 'events')
                 # Decide here which events to analyse
-                if (data[b'H1_PZ'][i] < 0) or (data[b'H2_PZ'][i] < 0) or (data[b'H3_PZ'][i] < 0):
+                if (data[b'H1_PZ'][i] < 0) or (data['H2_PZ'][i] < 0) or (data['H3_PZ'][i] < 0):
                     continue
                 # Fill arrays of events to be plotted and analysed further below
                 # Adding values for all three hadrons to the same variable here
                 pT.append(pT_H1[i])
                 pT.append(pT_H2[i])
                 pT.append(pT_H3[i])
-                pX.append(data[b'H1_PX'][i])
-                pX.append(data[b'H2_PX'][i])
-                pX.append(data[b'H3_PX'][i])
-                pY.append(data[b'H1_PY'][i])
-                pY.append(data[b'H2_PY'][i])
-                pY.append(data[b'H3_PY'][i])
-                pZ.append(data[b'H1_PZ'][i])
-                pZ.append(data[b'H2_PZ'][i])
-                pZ.append(data[b'H3_PZ'][i])
+                pX.append(data['H1_PX'][i])
+                pX.append(data['H2_PX'][i])
+                pX.append(data['H3_PX'][i])
+                pY.append(data['H1_PY'][i])
+                pY.append(data['H2_PY'][i])
+                pY.append(data['H3_PY'][i])
+                pZ.append(data['H1_PZ'][i])
+                pZ.append(data['H2_PZ'][i])
+                pZ.append(data['H3_PZ'][i])
     print(f"Read {event_counter:d} events")
 
     return [pT, pX, pY, pZ]
